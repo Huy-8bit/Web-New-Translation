d4516f1e6f846a7581b2c9a44aec9a812f67f183
Nguyễn Gia Huy <85545963+Huy-8bit@users.noreply.github.com>
2023-05-29 20:59:00 +0700

    new

--- 20127185_lab3.py
+++ 20127185_lab3.py
@@ -1,40 +1,78 @@
 import requests
 import openai
 from bs4 import BeautifulSoup
+from reportlab.pdfgen import canvas
+from reportlab.lib.pagesizes import letter
 
-# Split text into small token in order to handle large data content
+from reportlab.pdfbase import pdfmetrics
+from reportlab.pdfbase.ttfonts import TTFont
+
+from reportlab.platypus import SimpleDocTemplate, Paragraph
+from reportlab.lib.styles import getSampleStyleSheet
+
+
+
+font_path = "arial.ttf"
+
+
+pdfmetrics.registerFont(TTFont("Arial", font_path))
+
+
+
+
+
+def export_to_pdf(title, content):
+
+    pdf_file = f"{title}.pdf"
+    doc = SimpleDocTemplate(pdf_file, pagesize=letter)
+
+
+    elements = []
+
+    styles = getSampleStyleSheet()
+    elements.append(Paragraph(title, styles["Title"]))
+
+
+    styles = getSampleStyleSheet()
+    content_paragraph = Paragraph(content, styles["Normal"])
+    content_paragraph.fontName = "Arial" 
+    elements.append(content_paragraph)
+
+
+    doc.build(elements)
+
+    print(f"Exported to {pdf_file}")
+
+
+
+
+
+    
+
 def split_text(text, max_len):
-    # Split the text into words
+
     words = text.split()
     chunks = []
     chunk = ""
     for word in words:
-        # If adding the next word to the current chunk would exceed the maximum length,
-        # append the current chunk to the list of chunks and start a new chunk
+
         if len(chunk) + len(word) + 1 > max_len:
             chunks.append(chunk.strip())
             chunk = ""
-        # Otherwise, add the word to the current chunk
+
         chunk += " " + word
-    # Append the last chunk to the list of chunks
+
     if chunk:
         chunks.append(chunk.strip())
     return chunks
-
-# Define a function to translate a given text into a target language using OpenAI's GPT
 def translate(lang, text):
-    #key = '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'
-    #key = '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'
-    key = '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'
+    key = 'sk-yourkey'
     openai.api_key = key
     print("Translating to:", lang)
     print("Please wait ... ")
-    # Split the text into smaller chunks using a maximum token length of 990
     chunks = split_text(text, 990)
-    # Translate each chunk and append the translated text to a list
     translated_chunks = []
     for chunk in chunks:
-        # Use the translation API
         translation = openai.Completion.create(
             engine="text-davinci-002",
             prompt=f"Translate this {len(chunk)}-character text into {lang}: {chunk}",
@@ -44,14 +82,11 @@ def translate(lang, text):
             timeout=10,
             stop=None
         )
-        # Extract the translated text from the API response
         translated_text = translation.choices[0].text.strip()
         translated_chunks.append(translated_text)
-    # Join the translated chunks into a single string and return it
     translated_text = " ".join(translated_chunks)
     return translated_text
 
-# Define output in to txt file
 def save_to_file(lang,title, content, content_trans):
     f = open('Data.txt', 'w', encoding="utf-8")
     f.write("Title: ")
@@ -65,23 +100,20 @@ def save_to_file(lang,title, content, content_trans):
     f.write("\n")
     f.close()
 
-# Define a function to crawl content from a given number of pages on a website, translate the content,
-def crawlContent(nPage,lang):
+
+def crawlContent(nPage, lang):
     url = "https://vnexpress.net"
 
     req = requests.get(url)
     soup = BeautifulSoup(req.content, "html.parser", from_encoding='utf-8')
 
-    # find all article links in the homepage
     article_links = [a['href'] for i, a in enumerate(soup.select('h3.title-news a[href]')) if i < nPage]
     print(article_links)
 
-    # loop through each article link and extract the content
     for link in article_links:
         article_req = requests.get(link)
         article_soup = BeautifulSoup(article_req.content, "html.parser")
 
-        # extract the article title and content
         title = article_soup.find('h1', {'class': 'title-detail'})
         if title is None:
             break
@@ -91,39 +123,22 @@ def crawlContent(nPage,lang):
         string = ''.join(content)
         print("Title:", title)
 
-        # split content into chunks of 1000 tokens or less and translate each chunk
-        chunk_size = 1000
-        chunks = [string[i:i + chunk_size] for i in range(0, len(string), chunk_size)]
-        translated_chunks = [translate(lang, chunk) for chunk in chunks]
-
-        # join the translated chunks into a single string
-        translated_text = ''.join(translated_chunks)
-
-        # print the translated text
-        print("Translated content:", translated_text)
-        print('\n')
-
-        # Translate the content if it hasn't already been translated
         if not string.startswith("Translated to"):
-            # Split the content into chunks of 1000 tokens or less
             chunks = [string[i:i + 1000] for i in range(0, len(string), 1000)]
             translated_chunks = []
 
-            # Translate each chunk
             for chunk in chunks:
                 translated_chunk = translate(lang, chunk)
                 translated_chunks.append(translated_chunk)
 
-            # Combine the translated chunks into a single string
             translated_string = ''.join(translated_chunks)
 
-            # Print the translated text
             print("Translating is in progress. Please wait ... ")
             print(translated_string)
             print('\n')
 
-            # Write the translated text to a txt file
-    save_to_file(lang, title, content, translated_string)
+            export_to_pdf(title, translated_string)
+
 
 if __name__ == "__main__":
     lang = input("Enter language to translate: ")
